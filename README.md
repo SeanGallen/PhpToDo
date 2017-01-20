c6a955c340fd367168421ee1850c8c18dacafdd1
Sean <sjgallen@gmail.com>
2017-01-19 19:59:04 -0600

    init commit, created mysql connection and separate private code

---
+++ README.md
@@ -0,0 +1,5 @@
+Very simple To Do project.
+
+Using PHP, MYSQL and Javascript.
+
+You will need to add your own MYSQL connection. The secrets.php I include has my private info which I have gitignored.  Once you save those variables in the myssli_connection() you should have no problem.
---
+++ toDo.php
@@ -0,0 +1,19 @@
+<?php
+
+include 'secrets.php';
+
+$con = mysqli_connect($DB_SERVER, $DB_USER, $DB_PASS, $DB_NAME);
+if (!$con) {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+$sql1 = "create table People(id int not null, username text not null)";
+$sql2 = "insert into People(id, username) value (1, 'bob')";
+$sql3 = "Select * from People";
+mysqli_query($con,$sql1);
+mysqli_query($con,$sql2);
+$ans = mysqli_query($con,$sql3);
+
+printf("Select returned %d rows.\n", mysqli_num_rows($ans));
+
+mysqli_close($con);
+?>
