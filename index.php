6e77276192f1c5940ec99172cd0cc815adb33327
Sean <sjgallen@gmail.com>
2017-01-20 18:28:06 -0600

    index.php file and changed variable names in todo.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>PHP Todo</title>
+</head>
+<body>
+<section class="notePad">
+	<h2>Things to Do!</h2>
+	<input type="text" id="note" placeholder="Stuff...">
+</section>
+<ul class="sheets">
+	<li></li>
+</ul>
+</body>
+</html>
--- toDo.php
+++ toDo.php
@@ -2,15 +2,18 @@
 
 include 'secrets.php';
 
-$con = mysqli_connect($DB_SERVER, $DB_USER, $DB_PASS, $DB_NAME);
+$con = mysqli_connect($LOCALHOST, $USER_NAME, $PASSWORD, $DB_NAME);
 if (!$con) {
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
 }
+
 $sql1 = "create table People(id int not null, username text not null)";
 $sql2 = "insert into People(id, username) value (1, 'bob')";
 $sql3 = "Select * from People";
+
 mysqli_query($con,$sql1);
 mysqli_query($con,$sql2);
+
 $ans = mysqli_query($con,$sql3);
 
 printf("Select returned %d rows.\n", mysqli_num_rows($ans));
