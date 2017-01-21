f1c7f5648d442e8d4a14edf7d6af14183699aef9
Sean <sjgallen@gmail.com>
2017-01-20 20:11:58 -0600

    background photo and css added

--- index.php
+++ index.php
@@ -2,14 +2,18 @@
 <html>
 <head>
 	<title>PHP Todo</title>
+	<link rel="stylesheet" type="text/css" href="todo.css">
 </head>
 <body>
+<div class="postDiv">
 <section class="notePad">
 	<h2>Things to Do!</h2>
 	<input type="text" id="note" placeholder="Stuff...">
+	<span onclick="btnFunction">Note It</span>
 </section>
 <ul class="sheets">
 	<li></li>
 </ul>
+</div>
 </body>
 </html>
